a946cc17710f3ec41f3946c16b344ab54a3e499c
차지원 <chajiwon100785@gmail.com>
2020-05-16 13:31:03 +0900

    8, 9Week

---
+++ OpenCV_9Week/OpenCV_9Week/main.cpp
@@ -0,0 +1,14 @@
+#include<opencv2/opencv.hpp>
+#include<stdio.h>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+    Mat image(300,400,CV_8U,Scalar(255));
+    
+    
+    waitKey();
+    return 0;
+}
+
